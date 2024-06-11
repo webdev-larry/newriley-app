830cdfb8e2f8f4356b9272d421abb18051c7d958
web-dev larry <140874071+webdev-larry@users.noreply.github.com>
2024-06-11 06:22:13 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,5 +8,8 @@ apt-get update && apt-get install -y \
 # Install Composer dependencies
 composer install --no-dev --optimize-autoloader
 
+# Generate application key
+php artisan key:generate
+
 # Ensure correct permissions
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
